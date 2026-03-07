6066db3bcaca30cf75ea2c93bd7aa49e1a792bd7
4kumano <54392161+4kumano@users.noreply.github.com>
2026-03-07 18:03:21 +0800

    chore: Update project dependencies by adding new vendor files.

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-// Forward Vercel requests to normal Laravel public folder
+// Forward Vercel serverless requests to Laravel
+
+// Set the public path for static file serving
+$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../public';
+$_ENV['APP_RUNNING_IN_VERCEL'] = true;
+
 require __DIR__ . '/../public/index.php';
